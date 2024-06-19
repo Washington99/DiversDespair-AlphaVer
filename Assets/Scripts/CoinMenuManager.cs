3a04e51cbc851bf1d8721eac4a9139c924788914
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2024-06-19 18:43:58 +0800

    Made the coins accumulate and display in the main menu. It is also saved when the program is closed.

---
+++ Assets/Scripts/CoinMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinMenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI totalCoinsText;
+    private const string TotalCoinsKey = "TotalCoins";
+
+    void Start()
+    {
+        int totalCoins = PlayerPrefs.GetInt(TotalCoinsKey, 0);
+        totalCoinsText.text = ": " + totalCoins.ToString();
+    }
+}
+
