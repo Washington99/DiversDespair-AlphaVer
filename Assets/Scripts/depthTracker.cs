d69e8ea6279875d2b47876e8618cb5c42e64630b
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-06-19 22:43:00 +0800

    Implemented Score Multiplier Powerup

--- Assets/Scripts/depthTracker.cs
+++ Assets/Scripts/depthTracker.cs
@@ -10,12 +10,33 @@ public class depthTracker : MonoBehaviour
     [SerializeField] TextMeshProUGUI counter;
     public int points;
     float elapsedTime;
+    private int scoreMultiplier;
+    private float multiplierActiveTime;
+
+    void Start()
+    {
+        scoreMultiplier = 1;
+        multiplierActiveTime = 0.0f;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
+        elapsedTime += Time.deltaTime * scoreMultiplier;
         points = Mathf.FloorToInt(elapsedTime);
         counter.text =  "Depth:\n" + points * 10 + " m";
+
+        if (multiplierActiveTime > 0.0f)
+            multiplierActiveTime -= Time.deltaTime;
+
+        if (multiplierActiveTime <= 0.0f)
+            scoreMultiplier = 1;
+    }
+
+    public void SetScoreMultiplier(int multiplier)
+    {
+        scoreMultiplier = multiplier;
+        multiplierActiveTime = 15.0f;
     }
 
 //new
