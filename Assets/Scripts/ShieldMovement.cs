f80eaba452d1fbe364a71d1eaee440682cc55398
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-06-21 16:00:51 +0800

    Updated Shield Object
    Made it so that the shield object simply copies the transform of the active skin object

--- Assets/Scripts/ShieldMovement.cs
+++ Assets/Scripts/ShieldMovement.cs
@@ -5,60 +5,12 @@ using UnityEngine;
 
 public class ShieldMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private float maxSpeed;
-    private Rigidbody2D body;
-    private Vector2 screenBounds;
-
-    public bool isShieldPresent = false;
-
-    // [SerializeField] private int shieldDuration;
-
-    private bool isDead;
-
-    private Color spriteColor;
-
-    public GameManagerScript gameManager;
-
-    private void Awake()
-    {
-        body = GetComponent<Rigidbody2D>();
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        maxSpeed = speed;
-
-    }
+    public SkinSelector player;
 
     void Start()
     {
-        GetComponent<SpriteRenderer>().enabled = false;
-        spriteColor = GetComponent<SpriteRenderer>().color;
-    }
-
-    private void Update()
-    {
-        Move();
-        ClampVelocity();
-    }
-
-    private void Move()
-    {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed * 2);
-    }
-
-
-    private void ClampVelocity()
-    {
-        if (transform.position.x < -screenBounds.x || transform.position.x > screenBounds.x)
-            body.velocity = new Vector2(0, body.velocity.y);
-        if (transform.position.y < -screenBounds.y || transform.position.y > screenBounds.y)
-            body.velocity = new Vector2(body.velocity.x, 0);
-        if (body.velocity.y > maxSpeed)
-            body.velocity = new Vector2(body.velocity.x, maxSpeed);
-        if (body.velocity.y < -maxSpeed / 2)
-            body.velocity = new Vector2(body.velocity.x, -maxSpeed / 2);
+        gameObject.transform.parent = player.skins[PlayerPrefs.GetInt("SelectedSkin", 1)].transform;
+        ShieldOff();
     }
 
     public void ShieldOn()
